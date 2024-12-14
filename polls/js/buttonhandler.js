f2d4645be3a394d268cd9f3897e56980557f7b3b
SentyFunball <haydenbugatti@gmail.com>
2024-12-14 10:31:39 -0800

    christmas website

---
+++ polls/js/buttonhandler.js
@@ -0,0 +1,17 @@
+function userRedirect(where) {
+    window.location.href = `/polls/${where}`;
+}
+
+document.addEventListener("DOMContentLoaded", async() => {
+    const fadeables = document.querySelectorAll(".result");
+
+    //fade in elements
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            } 
+        });
+    });
+    fadeables.forEach((el) => observer.observe(el));
+});
